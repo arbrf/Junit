9b7397a17315d23e75d635d8b1d335ccd5af691e
Mohammad Shaik <133103759+arbrf@users.noreply.github.com>
2023-08-28 19:27:49 +0530

    Add files via upload

---
+++ MyMath.java
@@ -0,0 +1,12 @@
+package com.in28minutes.Junit;
+
+public class MyMath {
+	
+	public static void main (String args[]) {
+		MyMath objMath=new MyMath();
+		
+		int[] n= {1,2,3};
+		System.out.println(objMath.calculateSum(n));
+	}
+
+}
